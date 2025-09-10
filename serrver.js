49a391361919c695b3cc1d2715dd3cc957d86309
HuyHoangFSDev <nguyenthehuyhoang2002@gmail.com>
2025-09-10 17:32:55 +0700

    feat(project) init project

---
+++ serrver.js
@@ -0,0 +1,16 @@
+const app = require("./src/app");
+
+const PORT = process.env.DEV_APP_PORT || 3055;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  {
+    server.close(() => {
+      console.log("Server closed");
+    });
+    // notify.send( ping ...)
+  }
+});
---
+++ src/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const app = express();
+
+console.log("Process Environment:", process.env);
+
+// init middleware
+app.use(morgan("dev")); // hiên thị log request
+// morgan("combined");
+// morgan("common");
+// morgan("short");
+// morgan("tiny");
+
+app.use(helmet()); // bảo mật ứng dụng bằng cách thiết lập các tiêu đề HTTP
+
+app.use(compression()); // nén response để giảm băng thông
+
+// init db
+require("./dbs/init.mongodb");
+const { checkOverLoad } = require("./helpers/check.connect");
+checkOverLoad();
+
+// init routes
+app.use("/", require("./routes"));
+
+// hanlde errors
+
+module.exports = app;
